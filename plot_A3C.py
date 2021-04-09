259dcae5d75497913a7c039c8791537b155867fa
marfred <marfred@kth.se>
2021-04-09 18:18:58 +0200

    Updated plot functionality to match new code. Added mean plot

--- plot_A3C.py
+++ plot_A3C.py
@@ -2,126 +2,67 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-def import_data(directory):
-    """Imports data from directory. The format is an array with x_data as first element
-    and an instance of y_data for the subsequent elements. The labels are episodes, for
-    the x_data, and the filenames for the y_data."""
-    path = os.getcwd()
-    data_path = os.path.join(path, directory)
-    data_labels = ['episodes']
-    files_data = []     # entry 0 is episodes, the rest are score for every worker
-    for filename in os.listdir(data_path):
-        score = []
-        f = open(os.path.join(data_path, filename), 'r')
+def _import(session, i, plot_type):
+    worker_name = ('w%02i' % i) + '.txt'
+    data = []
+    for realization in os.listdir(session):
+        if os.path.isfile(realization):
+            continue
+        rel_path = os.path.join(session, realization)
+        data_path = os.path.join(rel_path, plot_type, worker_name)
+        data_list = []
+        f = open(data_path, 'r')
         while True:
-            curscore = f.readline().strip()
-            if curscore == '':
+            element = f.readline().strip()
+            if element == '':
                 break
-            score.append(float(curscore))
+            data_list.append(float(element))
         f.close()
-        if not files_data:
-            files_data.append(list(range(1, len(score) + 1)))
-        assert len(score) == len(files_data[0])
-        files_data.append(score)
-        data_labels.append(filename.split('.')[0])
-    return files_data, data_labels
+        data.append(data_list)
+    return data
 
-def import_data1(directory):
-    """Imports data from directory. The format is an array with x_data as first element
-    and an instance of y_data for the subsequent elements. The labels are episodes, for
-    the x_data, and the filenames for the y_data."""
-    path = os.getcwd()
-    data_path = os.path.join(path, directory)
-    data_labels = []
-    files_data = []     # entry 0 is episodes, the rest are score for every worker
-    x_data = []
-    for filename in os.listdir(data_path):
-        score = []
-        f = open(os.path.join(data_path, filename), 'r')
-        while True:
-            curscore = f.readline().strip()
-            if curscore == '':
-                break
-            score.append(float(curscore))
-        f.close()
-        x_data.append(list(range(1, len(score) + 1)))
-        files_data.append(score)
-        data_labels.append(filename.split('.')[0])
-    return x_data, files_data, data_labels
 
-def plot(data, title='', xlabel='', ylabel='', labels=None, show=True, filename=False):
-    """Plots the data. Data and labels have to be in the same form as output from import_data()."""
+def average(session, plot_type):
+    f = open(os.path.join(session, 'config.txt'), 'r')
+    while True:
+        line = f.readline().split(':')
+        if line[0].strip() == 'number of threads':
+            n = int(float(line[1].strip()))
+            break
+    f.close()
+    average = []
+    for i in range(n):
+        worker = np.array(_import(session, i, plot_type))
+        mean = np.mean(worker, axis=0)
+        average.append(mean)
+    mean = np.mean(average, axis=0)
+    std = np.std(average, axis=0)
+    return mean, std
 
-    # Font sizes in plots
+
+def plot(mean, std, type, show=True, filename=False):
     title_font = {'fontname': 'Arial', 'size': '20',
                       'color': 'black', 'weight': 'normal'}
     axis_font = {'fontname': 'Arial', 'size': '18'}
-
-    # Plot score as a function of episodes.
-    # Study variance and progress here.
+    sigma_upper = mean + std
+    sigma_lower = mean - std
     plt.figure()
-    plt.title(title)
-    plt.xlabel(xlabel, **axis_font)
-    plt.ylabel(ylabel, **axis_font)
-    plt.legend()
-    if labels:
-        for i in range(1, len(data)):
-            plt.plot(data[0], data[i], label=labels[i])
-        plt.legend()
+    if type == 'probability':
+        plt.title('mean ' + 'probability' + ' vs ' + 'steps', **title_font)
+        plt.xlabel('step', **axis_font)
     else:
-        for i in range(1, len(data)):
-            plt.plot(data[0], data[i])
+        plt.title('mean ' + type + ' vs ' + 'episodes', **title_font)
+        plt.xlabel('episodes', **axis_font)
+    plt.ylabel('mean' + type, **axis_font)
+    labels = ['plus sigma', 'mean ' + type, 'minus sigma']
+    y_data = [sigma_upper, mean, sigma_lower]
+    for i in range(3):
+        plt.plot([j for j in range(1, len(y_data[i]) + 1)], y_data[i], label=labels[i])
+    plt.legend()
     if filename:
         plt.savefig(filename)
     if show:
         plt.show()
 
-def plot1(x_data, y_data, title='', xlabel='', ylabel='', labels=None, show=True, filename=False):
-    """Plots the data. Data and labels have to be in the same form as output from import_data()."""
-
-    # Font sizes in plots
-    title_font = {'fontname': 'Arial', 'size': '20',
-                      'color': 'black', 'weight': 'normal'}
-    axis_font = {'fontname': 'Arial', 'size': '18'}
-
-    # Plot score as a function of episodes.
-    # Study variance and progress here.
-    plt.figure()
-    plt.title(title)
-    plt.xlabel(xlabel, **axis_font)
-    plt.ylabel(ylabel, **axis_font)
-    plt.legend()
-    if labels:
-        for i in range(1, len(x_data)):
-            plt.plot(x_data[i], y_data[i], label=labels[i])
-        plt.legend()
-    else:
-        for i in range(1, len(x_data)):
-            plt.plot(x_data[i], y_data[i])
-    if filename:
-        plt.savefig(filename)
-    if show:
-        plt.show()
-
-def meanscore(data, k):
-    """Creates meanscore data. Data has to be in the same form as output from import_data().
-    The data output has the same format as input."""
-    meandata = [data[0][k:len(data[0]) - k]]
-    for i in range(1, len(data)):
-        curr_data = []
-        for j in range(k, len(data[i]) - k):
-            curr_data.append(np.mean(data[i][j - k:k + j]))
-        meandata.append(curr_data)
-    return meandata
-
-
-data, labels = import_data('plot_fold')
-mean_data = meanscore(data, 10)
-#plot(data, title='Reward plot', xlabel='x', ylabel='y', labels=labels, show=False)
-plot(mean_data, title='Mean reward plot', xlabel='x', ylabel='y', labels=labels, show=False)
-
-data1, labels1 = import_data('conv_plot_fold')
-plot(data1, title='Convergence plot', xlabel='x', ylabel='y', labels=labels1, show=False)
-
-x_data, y_data, labels2 = import_data1('prob_plot_fold')
-plot1(x_data, y_data, title='Probability plot', xlabel='x', ylabel='y', labels=labels2, show=True)
+mean, std = average('Session 1', 'score_plot')
+plot(mean, std, 'score')
