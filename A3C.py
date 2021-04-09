f6e22363496453cc35d600b95f85c53a0e543c94
marfred <marfred@kth.se>
2021-04-09 15:43:35 +0200

    Improved documentation for these files

--- A3C.py
+++ A3C.py
@@ -10,7 +10,7 @@ import pickle
 
 
 def t(m_array):
-    """Converts a multidimensional array to a torch tensor and returns it."""
+    """Converts a multidimensional array to a torch tensor."""
     return torch.Tensor(m_array)
 
 
@@ -18,9 +18,10 @@ class SharedAdam(torch.optim.Adam):
     """An Adam optimizer shared by all workers."""
     def __init__(self, params, lr=1e-4, betas=(0.9, 0.999), eps=1e-8,
             weight_decay=0):
+        """Creates an Adam optimizer."""
         super(SharedAdam, self).__init__(params, lr=lr, betas=betas, eps=eps,
                 weight_decay=weight_decay)
-
+        # Comment
         for group in self.param_groups:
             for p in group['params']:
                 state = self.state[p]
@@ -34,8 +35,8 @@ class SharedAdam(torch.optim.Adam):
 
 class Worker(mp.Process):
     """A single reinforcement agent worker."""
-
     def __init__(self, parameters, i):
+        """Creates a worker."""
         super(Worker, self).__init__()
 
         # Environment, network, optimizer
@@ -50,8 +51,10 @@ class Worker(mp.Process):
         self.entropy_reg_factor = parameters['entropy regularization factor']
         self.gamma = parameters['gamma']
 
-        # Miscellaneous
+        # Memory
         self.memory = Memory()
+
+        # Worker id and episode index
         self.episode_idx = parameters['global episode index']
         self.name = 'w%02i' % i
 
@@ -63,16 +66,22 @@ class Worker(mp.Process):
         self.data = Data_storage(parameters['rel_path'], parameters['environment name'])
 
     def run(self):
-        for i in range(self.max_episodes):
+        """The tasks of a single worker.
+        The workers interact with the environment here."""
+        for i in range(self.max_episodes): # For every episode
+            # Reset the parameters and the environment
             done = False
             total_reward = 0
             observation = self.env.reset()
             steps = 1
-            render_ep = 10
+
+            # Comment the five lines below to render the episodes
+            #render_ep = 10
             #if i % render_ep == 0:
             #    render = True
             #else:
             #    render = False
+
             while not done: # Until episode is done
                 # Sample an action based on state
                 state = torch.tensor([observation], dtype=torch.float)
@@ -80,120 +89,154 @@ class Worker(mp.Process):
                 probs = torch.softmax(probs, dim=1)
                 dist = torch.distributions.Categorical(probs)
                 action = dist.sample().numpy()[0]
+
                 # Simulate environment
                 observation_, reward, done, info = self.env.step(action)
-                accumulated_reward = reward
-                for i in range(3):
-                    if done:
-                        break
-                    observation_, reward, done, info = self.env.step(0)
-                    accumulated_reward += reward
+                total_reward += reward
 
-                total_reward += accumulated_reward
                 # Add data to memory
                 self.memory.add(observation, action, accumulated_reward, value)
+
+                # Add probability data to the data storage object
                 self.data.add_prob(probs)
-                # Call training
-                if steps % self.t_max == 0 or done:
+
+                if steps % self.t_max == 0 or done: # If episode is done or
+                                                    # tmax is reached
                     # Train locally
                     self.train(done)
 
-                    # Update global networks and copy to local
+                    # Update the global network and copy it to local network
                     self.update_global()
 
-                    # Reset memory
+                    # Reset the memory
                     self.memory.clear()
 
-                # Iterate to next state
+                # Iterate to the next state
                 steps += 1
                 observation = observation_
+
+                # Comment the three lines below to render the episodes
                 #if render:
                 #    self.env.render()
                 #    time.sleep(1/30)
+
+            # Increment the episode index
             with self.episode_idx.get_lock():
                 self.episode_idx.value += 1
+
+            # Print the result
             print(self.name, 'episode', self.episode_idx.value, total_reward)
+
+            # Add score and conv data to the data storage object
             self.data.add_score(total_reward)
             self.data.add_conv(self.local_actor_critic)
 
-            if len(self.data.score_plot) > 100000:
+
+            if i % 100000: # For every 100 000 episodes
+                # Save data in textfiles
                 self.data.save_score(self.name)
                 self.data.save_conv(self.name)
                 self.data.save_prob(self.name)
-                self.data.clear_plot()
+
+                # Clear data storage
+                self.data.clear_data()
+
+        ### At the end of the session: ###
+
+        # Save data in textfiles
         self.data.save_score(self.name)
         self.data.save_conv(self.name)
         self.data.save_prob(self.name)
-        self.data.clear_plot()
+
+        # Clear data storage
+        self.data.clear_data()
+
+        # Save the global network in a textfile when worker 00 terminates
         if self.name == 'w00':
             self.data.save_net(self.global_actor_critic)
 
     def train(self, done):
-        """Trains the neural networks (actor and critic) based on the
-            memory of the previous episode."""
-
+        """Train the neural networks (actor and critic) based on the
+            recent memory."""
+        # Convert states and actions to tensors
         states = torch.tensor(self.memory.states, dtype=torch.float)
         actions = torch.tensor(self.memory.actions, dtype=torch.float)
 
+        # Calculate the action distributions and values
         probs, values = self.local_actor_critic.forward(states)
 
-        R = values[-1]*(1-int(done))
+        # Approximate the value function of the last state
+        R = values[-1] * (1 - int(done))
 
         batch_return = []
-        for reward in self.memory.rewards[::-1]:
-            R = reward + self.gamma*R
+        for reward in self.memory.rewards[::-1]: # For every state
+                                                 # (travesing backwards)
+            # Calculate the discounted total reward
+            R = reward + self.gamma * R
             batch_return.append(R)
-        batch_return.reverse()
-        batch_return = torch.tensor(batch_return, dtype=torch.float)
 
+        # Reverse the list of returns
+        batch_return.reverse()
+
+        # Convert the list of returns to a tensor
+        batch_return = torch.tensor(batch_return, dtype=torch.float)
         returns = batch_return
 
+        # Calculate the critic loss
         values = values.squeeze()
         critic_loss = (returns-values)**2
 
+        # Calculate the log probabilites
         probs = torch.softmax(probs, dim=1)
         dist = torch.distributions.Categorical(probs)
         log_probs = dist.log_prob(actions)
+
+        # Calculate the actor loss
         actor_loss = -log_probs*(returns-values)
+
+        # Calculate the entropy
         entropy = -torch.mul(probs, torch.log(probs)).mean()
+
+        # Calculate the total loss
         total_loss = (critic_loss + actor_loss).mean() - entropy * self.entropy_reg_factor
 
-        #for param in self.local_actor_critic.parameters():
-        #    square = param.data.pow(2)
-        #    total_loss = total_loss + torch.mean(square) * 20
-
+        # Reset the gradient and propagate the loss
         self.optimizer.zero_grad()
         total_loss.backward()
 
     def update_global(self):
-        """Updates the global networks."""
+        """Update the global network based on the gradient of the worker."""
         # Transfer gradient
         for local_param, global_param in zip(self.local_actor_critic.parameters(),
                                              self.global_actor_critic.parameters()):
             global_param._grad = local_param.grad
-        # Updates global network based on gradients
+
+        # Update the global network based on gradient
         self.optimizer.step()
-        # Copies global network to local one
+
+        # Copy the global network to the local network
         self.local_actor_critic.load_state_dict(
             self.global_actor_critic.state_dict())
 
 
 class Memory():
-    """The memory of a worker."""
-
+    """The memory of a single worker."""
     def __init__(self):
+        """Creates attributes."""
         self.states = []
         self.actions = []
         self.rewards = []
         self.values = []
 
     def add(self, state, action, reward, value):
+        """Add experience to the memory."""
         self.states.append(state)
         self.actions.append(action)
         self.rewards.append(reward)
         self.values.append(value)
 
     def clear(self):
+        """Clear the memory."""
         self.states = []
         self.actions = []
         self.rewards = []
@@ -201,8 +244,8 @@ class Memory():
 
 
 class Data_storage():
-    """Stores and loads data not directly required for the algorithm to run."""
-
+    """Store data not directly required for the algorithm to run."""
+    """Non of the methods are required for the algorithm to function."""
     def __init__(self, path, env_name):
 
         # path for data storage
@@ -216,15 +259,18 @@ class Data_storage():
         self.conv_plot = []
         self.prob_plot = []
 
-    def clear_plot(self):
+    def clear_data(self):
+        """Clear the data."""
         self.score_plot = []
         self.conv_plot = []
         self.prob_plot = []
 
     def add_score(self, total_reward):
+        """Add score data."""
         self.score_plot.append(total_reward)
 
     def add_conv(self, local_actor_critic):
+        """Add convergence data."""
         temp_list = []
         for state in self.test_states:
             _, value = local_actor_critic(t(state))
@@ -232,9 +278,11 @@ class Data_storage():
         self.conv_plot.append(torch.mean(t(temp_list)))
 
     def add_prob(self, probs):
+        """Add probability data."""
         self.prob_plot.append(probs.squeeze()[0])
 
     def save_score(self, name):
+        """Save the score data in a textfile."""
         folderpath = os.path.join(self.path, 'score_plot')
         filepath = os.path.join(folderpath, name + '.txt')
         f = open(filepath, 'a+')
@@ -243,6 +291,7 @@ class Data_storage():
         f.close()
 
     def save_conv(self, name):
+        """Save the convergence data in a textfile."""
         folderpath = os.path.join(self.path, 'conv_plot')
         filepath = os.path.join(folderpath, name + '.txt')
         f = open(filepath, 'a+')
@@ -251,6 +300,7 @@ class Data_storage():
         f.close()
 
     def save_prob(self, name):
+        """Save the probability data in a textfile."""
         folderpath = os.path.join(self.path, 'prob_plot')
         filepath = os.path.join(folderpath, name + '.txt')
         f = open(filepath, 'a+')
@@ -259,14 +309,15 @@ class Data_storage():
         f.close()
 
     def save_net(self, network):
+        """Save the network in a textfile."""
         filepath = os.path.join(self.path, 'network.txt')
         f = open(filepath, 'wb')
         pickle.dump(network, f)
         f.close()
 
     def _teststates(self, env_name):
-        """Generates test states for the convergence plot dependent on the environment."""
-
+        """Generate test states for the convergence plot
+        dependent on the environment (FlappyBird or CartPole)."""
         teststates = []
         if env_name == 'FlappyBird-v0':
             for i in range(0, 280, 28):
@@ -282,60 +333,59 @@ class Data_storage():
 
 
 class ActorCritic(nn.Module):
-    """A neural network which works as a function approximator for the policy."""
-
+    """A neural network which works as a function approximator
+    for the policy and value function."""
     def __init__(self, state_dim, n_actions):
-        """Initiates the neural network."""
+        """Create the neural network."""
         super().__init__()
-        #self.pi1 = nn.Linear(*state_dim, 128)
-        #self.v1 = nn.Linear(*state_dim, 128)
-        #self.pi = nn.Linear(64, n_actions)
-        #self.v = nn.Linear(64, 1)
         self.first = nn.Linear(*state_dim, 128)
         self.critic_mid = nn.Linear(128, 64)
         self.actor_mid = nn.Linear(128, 64)
         self.critic = nn.Linear(64, 1)
         self.actor = nn.Linear(64, n_actions)
+
     def forward(self, state):
-        """Samples an action from the policy based on the current state."""
-        #pi1 = F.relu(self.pi1(state))
-        #v1 = F.relu(self.v1(state))
-
-        #pi = self.pi(pi2)
-        #v = self.v(pi2)
-
+        """Sample an action distribution from the policy and the value
+        function approximation based on the current state."""
         first = F.relu(self.first(state))
         actor_mid = torch.tanh(self.actor_mid(first))
         critic_mid = torch.tanh(self.critic_mid(first))
         actor = self.actor(actor_mid)
         critic = self.critic(critic_mid)
-
-        #return pi, v
         return actor, critic
 
 
 def main(parameters, network=None):
-    """Initializes relevant parameters and starts the workers."""
-
-    # Calculates state- and action space dimensions
+    """Initialize relevant parameters and start the workers."""
+    # Create a temporary environment
     temp_env = gym.make(parameters['environment name'])
 
-    # Creates a global network or loads a new one
+    # Create a global network or loads a new one
     if network:
         parameters['global actor critic'] = network
     else:
-        parameters['global actor critic'] = ActorCritic([temp_env.observation_space.shape[0]], temp_env.action_space.n)
+        parameters['global actor critic'] = ActorCritic(
+        [temp_env.observation_space.shape[0]], # Dimension of observation space
+        temp_env.action_space.n # Dimension of action space
+        )
 
-    # Shares the memory of the global network
+    # Share the memory of the global network
     parameters['global actor critic'].share_memory()
 
-    # Creates a shared Adam
-    parameters['optimizer'] = SharedAdam(parameters['global actor critic'].parameters(), lr=parameters['learning rate'])
+    # Create a shared Adam optimizer
+    parameters['optimizer'] = SharedAdam(
+    parameters['global actor critic'].parameters(),
+    lr=parameters['learning rate']
+    )
 
-    # Global episode count
+    # Create global episode count
     parameters['global episode index'] = global_ep = mp.Value('i', 0)
 
+    # Create workers
     workers = [Worker(parameters, i)
                for i in range(parameters['number of threads'])]
+    # Start the workers
     [w.start() for w in workers]
+
+    # Wait for workers to finish
     [w.join() for w in workers]
--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import pickle
 
 
 def read_config(filename):
-    """Reads parameters from the config file and converts them to the proper data type.
-     Returns the parameters in a dictionary."""
-
+    """Read parameters from the config file and convert them
+    to the proper data type. Returns the parameters in a dictionary."""
     f = open(filename, 'r')
     parameters = {}
     while True:
@@ -20,7 +19,8 @@ def read_config(filename):
         value = variable_list[1].strip()
         if name in ['learning rate', 'gamma', 'entropy regularization factor']:
             parameters[name] = float(value)
-        elif name in ['t_max', 'max episodes', 'number of threads', 'number of realizations', 'max load episodes']:
+        elif name in ['t_max', 'max episodes', 'number of threads',
+                     'number of realizations', 'max load episodes']:
             parameters[name] = int(float(value))
         else:
             parameters[name] = value
@@ -29,14 +29,14 @@ def read_config(filename):
 
 
 def load_session(path, max_episodes):
-    """Imports parameters and networks from an old session and continues where it ended. It trains every realization an
-    additional max_episodes times."""
-
-    # Imports parameters and set max episodes
+    """Import parameters and networks from an old session and continue
+    where it ended. Train every realization an additional
+    max_episodes times."""
+    # Import parameters and set max episodes
     parameters = read_config(os.path.join(path, 'config.txt'))
     parameters['max episodes'] = max_episodes
 
-    # Continues the algorithm for every realization in the loaded session
+    # Continue the algorithm for every realization in the loaded session
     for realization in os.listdir(path):
         if os.path.isfile(realization):
             continue
@@ -49,12 +49,11 @@ def load_session(path, max_episodes):
 
 
 def new_session(parameters, path):
-    """Creates a new session and runs the algorithm once per realization."""
-
+    """Create a new session and run the algorithm once per realization."""
     # Create root folder
     os.mkdir(path)
 
-    # Add the config
+    # Create the config
     f = open(os.path.join(path, 'config.txt'), 'w+')
     for key, value in parameters.items():
         if key == 'load session':
@@ -66,19 +65,20 @@ def new_session(parameters, path):
     # Create realization folders and run A3C for every realization
     for nr in range(1, parameters['number of realizations'] + 1):
         rel_path = os.path.join(path, 'realization_' + str(nr))
+
         # Create folders
         os.mkdir(rel_path)
         for folder in ['score_plot', 'conv_plot', 'prob_plot']:
             os.mkdir(os.path.join(rel_path, folder))
+
         # Run A3C for every realization
         parameters['rel_path'] = rel_path
         A3C.main(parameters)
 
 
 def main():
-    """Runs a new session or continues an old one depending on the
+    """Run a new session or continue an old one depending on the
     specifications in the config."""
-
     # Read parameters from config file
     parameters = read_config('config.txt')
     main_path = parameters['session name']
